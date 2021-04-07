509e53dc9510035a51da8b8aebf1bade544f5946
LonnieQin <354060093@qq.com>
2021-04-08 01:25:29 +0800

    Code refactor.

--- Shared/Models/AppModel.swift
+++ Shared/Models/AppModel.swift
@@ -7,45 +7,6 @@
 
 import SwiftUI
 
-@propertyWrapper
-public class CodableAppStorage<T: Codable> {
-    
-    var encoder: JSONEncoder
-    
-    var decoder: JSONDecoder
-    
-    var storage: UserDefaults
-    
-    var key: String
-    
-    public init(
-        _ key: String,
-        encoder: JSONEncoder = JSONEncoder(),
-        decoder: JSONDecoder = JSONDecoder(),
-        storage: UserDefaults = .standard
-    ) {
-        self.key = key
-        self.encoder = encoder
-        self.decoder = decoder
-        self.storage = storage
-    }
- 
-    public var wrappedValue: T? {
-        get {
-            if let data = storage.data(forKey: key) {
-                return try? decoder.decode(T.self, from: data)
-            }
-            return nil
-        }
-        set {
-            if let data = try? encoder.encode(newValue) {
-                storage.set(data, forKey: key)
-            }
-        }
-    }
-
-}
-
 class AppModel: ObservableObject {
     
     var names: [Icon] = []
